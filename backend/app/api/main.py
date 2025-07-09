70745f6020ecd9f1ca6a835a075fdec242c08f67
himura467 <mitarashidango0927@gmail.com>
2025-07-10 03:42:21 +0900

    feat: api main

---
+++ backend/app/api/main.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter
+from api.routes import account, admin, auth, event, verify
+
+api_router = APIRouter()
+
+api_router.include_router(
+    account.router,
+    prefix="/accounts",
+    tags=["accounts"],
+)
+
+api_router.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+)
+
+api_router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["auth"],
+)
+
+api_router.include_router(
+    event.router,
+    prefix="/events",
+    tags=["events"],
+)
+
+api_router.include_router(
+    verify.router,
+    prefix="/verify",
+    tags=["verify"],
+)
