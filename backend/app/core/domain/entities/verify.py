90cd0d53fec46d4a6bfb0630e569e0ee2ff10973
himura467 <mitarashidango0927@gmail.com>
2025-07-10 01:48:55 +0900

    feat: domain/entities/

---
+++ backend/app/core/domain/entities/verify.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from pydantic.networks import EmailStr
+
+from core.domain.entities.base import IEntity
+from core.utils.uuid import UUID
+
+
+class EmailVerification(IEntity):
+    def __init__(
+        self,
+        entity_id: UUID,
+        email: EmailStr,
+        verification_token: UUID,
+        token_expires_at: datetime,
+    ) -> None:
+        super().__init__(entity_id)
+        self.email = email
+        self.verification_token = verification_token
+        self.token_expires_at = token_expires_at
