690835f5fe4e0852a2195e800cf9097e92b87041
himura467 <mitarashidango0927@gmail.com>
2025-07-10 03:15:31 +0900

    feat: routes/

---
+++ backend/app/api/routes/account.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from core.dtos.account import (
+    CreateUserAccountRequest,
+    CreateUserAccountResponse,
+    GetFollowersInfoResponse,
+)
+from core.features.account import Account, Role
+from core.infrastructure.sqlalchemy.db import get_db_async
+from core.infrastructure.sqlalchemy.unit_of_work import SqlalchemyUnitOfWork
+from core.usecase.account import AccountUsecase
+from api.deps import AccessControl
+
+router = APIRouter()
+
+
+@router.post(
+    path="/create", name="Create User Account", response_model=CreateUserAccountResponse
+)
+async def create_user_account(
+    req: CreateUserAccountRequest, session: AsyncSession = Depends(get_db_async)
+) -> CreateUserAccountResponse:
+    username = req.username
+    password = req.password
+    nickname = req.nickname
+    birth_date = req.birth_date
+    gender = req.gender
+    email = req.email
+    followee_usernames = req.followee_usernames
+
+    uow = SqlalchemyUnitOfWork(session=session)
+    usecase = AccountUsecase(uow=uow)
+
+    return await usecase.create_user_account_async(
+        username=username,
+        password=password,
+        nickname=nickname,
+        birth_date=birth_date,
+        gender=gender,
+        email=email,
+        followee_usernames=set(followee_usernames),
+    )
+
+
+@router.get(
+    path="/followers",
+    name="Get Followers Info",
+    response_model=GetFollowersInfoResponse,
+)
+async def get_followers_info(
+    session: AsyncSession = Depends(get_db_async),
+    account: Account = Depends(AccessControl(permit={Role.HOST})),
+) -> GetFollowersInfoResponse:
+    uow = SqlalchemyUnitOfWork(session=session)
+    usecase = AccountUsecase(uow=uow)
+
+    return await usecase.get_followers_info_async(followee_id=account.account_id)
