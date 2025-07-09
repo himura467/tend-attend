46832c90d68d3fcb33b0e2a3889d5c8320507315
himura467 <mitarashidango0927@gmail.com>
2025-07-10 03:43:00 +0900

    feat: app main

---
+++ backend/app/main.py
@@ -0,0 +1,28 @@
+from typing import Awaitable, Callable
+
+from fastapi import FastAPI, Request, Response, status
+from mangum import Mangum
+from starlette.middleware.base import BaseHTTPMiddleware
+from api.main import api_router
+
+app = FastAPI()
+
+
+class CORSMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    ) -> Response:
+        if request.method == "OPTIONS":
+            response = Response(status_code=status.HTTP_204_NO_CONTENT)
+            response.headers["Access-Control-Allow-Headers"] = (
+                "content-type, x-amz-content-sha256"
+            )
+            return response
+        return await call_next(request)
+
+
+app.add_middleware(CORSMiddleware)
+
+app.include_router(api_router)
+
+lambda_handler = Mangum(app)
