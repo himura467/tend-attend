698872da5c89c326a111619cfc46305de4a4057e
himura467 <mitarashidango0927@gmail.com>
2025-07-13 09:51:28 +0900

    fix: working directory where export_requirements.sh runs

--- scripts/export_requirements.sh
+++ scripts/export_requirements.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 ROOT_DIR=$(cd "$(dirname "$0")"/..; pwd)
 
+cd backend
 uv export --format requirements.txt -o requirements.txt --no-dev --no-hashes
+mv requirements.txt "$ROOT_DIR/requirements.txt"
