d163fae1276d5c41c198d51c052b5b97703ac7bc
himura467 <mitarashidango0927@gmail.com>
2025-07-17 21:22:11 +0900

    feat: cloudfront output distribution arn

--- terraform/modules/cloudfront/outputs.tf
+++ terraform/modules/cloudfront/outputs.tf
@@ -1,3 +1,8 @@
+output "distribution_arn" {
+  description = "ARN of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.this.arn
+}
+
 output "distribution_domain_name" {
   description = "Domain name of the CloudFront distribution"
   value       = aws_cloudfront_distribution.this.domain_name
