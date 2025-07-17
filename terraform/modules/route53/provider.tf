d6046c63fe672de8c3e8685544a007518cda7908
himura467 <mitarashidango0927@gmail.com>
2025-07-17 20:02:13 +0900

    feat: prod aws provider

--- terraform/modules/route53/provider.tf
+++ terraform/modules/route53/provider.tf
@@ -1,8 +1,6 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "6.3.0"
       configuration_aliases = [aws, aws.us_east_1]
     }
   }
