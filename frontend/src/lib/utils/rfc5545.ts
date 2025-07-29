4c60cd7d9dd6fb7d109fdbcc38c2985c10e43795
himura467 <mitarashidango0927@gmail.com>
2025-07-29 16:19:51 +0900

    feat: change the return value of `parseRecurrence` from RRule to RRuleSet

--- frontend/src/lib/utils/rfc5545.ts
+++ frontend/src/lib/utils/rfc5545.ts
@@ -1,7 +1,13 @@
-import { RRule } from "rrule";
+import { RRuleSet, rrulestr } from "rrule";
 
-export const parseRecurrence = (recurrences: string[]): RRule | null => {
+export const parseRecurrence = (recurrences: string[]): RRuleSet | null => {
   if (recurrences.length === 0) return null;
   const rfcString = recurrences.join("\n");
-  return RRule.fromString(rfcString);
+  const result = rrulestr(rfcString);
+  if (result instanceof RRuleSet) {
+    return result;
+  }
+  const rruleSet = new RRuleSet();
+  rruleSet.rrule(result);
+  return rruleSet;
 };
