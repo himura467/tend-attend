084dbdbcb488a095247b03a0adc1dac111fd9162
himura467 <mitarashidango0927@gmail.com>
2025-11-02 00:46:06 +0900

    i18n: title for SignUpPage

--- frontend/src/app/[locale]/signup/page.tsx
+++ frontend/src/app/[locale]/signup/page.tsx
@@ -2,15 +2,20 @@ import { SignUpForm } from "@/components/organisms/specific/accounts/signup/Sign
 import { DialogTemplate } from "@/components/templates/DialogTemplate";
 import { rr } from "@/lib/utils/reverseRouter";
 import { NextPage } from "next";
+import { Locale, useTranslations } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
 import Link from "next/link";
 import React from "react";
 
-const SignUpPage: NextPage = (): React.JSX.Element => {
+const SignUpPage: NextPage = ({ params }: PageProps<"/[locale]">): React.JSX.Element => {
+  const { locale } = React.use(params);
+  setRequestLocale(locale as Locale);
+  const t = useTranslations("SignUpPage");
   return (
     <DialogTemplate>
       <div className="w-full max-w-[600px] space-y-6">
         <div className="text-center">
-          <h1 className="text-foreground text-3xl font-bold tracking-tight sm:text-4xl">Create your account</h1>
+          <h1 className="text-foreground text-3xl font-bold tracking-tight sm:text-4xl">{t("title")}</h1>
           <p className="text-muted-foreground mt-2">
             Already have an account?{" "}
             <Link {...rr.signin.index()} className="text-primary font-medium hover:underline">
