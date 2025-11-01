be0f7f3086813addcbe1d3433fd50c0c72c5991a
himura467 <mitarashidango0927@gmail.com>
2025-11-02 06:23:54 +0900

    feat: enable DNS for VPC

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -13,7 +13,9 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "private" {
