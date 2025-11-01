cae00984f3ae2e65b938aa2956087de9ad7bfbe4
himura467 <mitarashidango0927@gmail.com>
2025-11-02 07:16:01 +0900

    feat: route table associations

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -69,3 +69,15 @@ resource "aws_route" "public_default" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.this.id
 }
+
+resource "aws_route_table_association" "private" {
+  count          = length(data.aws_availability_zones.available.names)
+  route_table_id = aws_route_table.private[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(data.aws_availability_zones.available.names)
+  route_table_id = aws_route_table.public[count.index].id
+  subnet_id      = aws_subnet.public[count.index].id
+}
