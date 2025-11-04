1927c46a1ca9dfcc9f9a4be4d29a1d001d68e294
himura467 <mitarashidango0927@gmail.com>
2025-11-04 14:47:30 +0900

    refactor: use recommended ingress and egress rule format

--- terraform/modules/security_group/private/main.tf
+++ terraform/modules/security_group/private/main.tf
@@ -1,28 +1,32 @@
 resource "aws_security_group" "lambda" {
   name   = "tend-attend-lambda-sg"
   vpc_id = var.vpc_id
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    ipv6_cidr_blocks = ["::/0"]
-  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "lambda_ipv6" {
+  security_group_id = aws_security_group.lambda.id
+  description       = "Allow all outbound IPv6 traffic"
+  ip_protocol       = "-1"
+  cidr_ipv6         = "::/0"
 }
 
 resource "aws_security_group" "aurora" {
   name   = "tend-attend-aurora-sg"
   vpc_id = var.vpc_id
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda.id]
-    description     = "Allow MySQL access from Lambda functions"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "aurora_mysql" {
+  security_group_id            = aws_security_group.aurora.id
+  description                  = "Allow MySQL access from Lambda functions"
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.lambda.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "aurora_ipv4" {
+  security_group_id = aws_security_group.aurora.id
+  description       = "Allow all outbound IPv4 traffic"
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
