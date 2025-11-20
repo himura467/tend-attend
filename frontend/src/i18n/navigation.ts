2482083f6bf8db7704822e27c3d7f462e5016813
himura467 <mitarashidango0927@gmail.com>
2025-11-20 21:36:22 +0900

    feat: next-intl compatible LinkProps

--- frontend/src/i18n/navigation.ts
+++ frontend/src/i18n/navigation.ts
@@ -1,4 +1,6 @@
 import { routing } from "@/i18n/routing";
 import { createNavigation } from "next-intl/navigation";
+import React from "react";
 
 export const { Link, getPathname, redirect, usePathname, useRouter } = createNavigation(routing);
+export type LinkProps = React.ComponentProps<typeof Link>;
