998907f829ffc9a134680fb81c431955f0f929df
himura467 <mitarashidango0927@gmail.com>
2025-11-29 20:08:54 +0900

    feat: BACKEND_API_URL

--- frontend/src/lib/utils/url.ts
+++ frontend/src/lib/utils/url.ts
@@ -1,5 +1,7 @@
 import url from "url";
 
+export const BACKEND_API_URL = process.env.BACKEND_API_URL || "http://localhost:8000";
+
 export const formatUrl = (urlObject: string | url.UrlObject): string => {
   return url.format(urlObject);
 };
