e8844528158fc462dc722d0724aad453f661c4b8
himura467 <mitarashidango0927@gmail.com>
2025-12-19 03:50:22 +0900

    fix: set Access-Control-Allow-Methods for OPTIONS request

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,14 +14,14 @@ class CORSMiddleware(BaseHTTPMiddleware):
         if request.method == "OPTIONS":
             response = Response(status_code=status.HTTP_204_NO_CONTENT)
             response.headers["Access-Control-Allow-Headers"] = "content-type, x-amz-content-sha256, x-basic-auth"
+            response.headers["Access-Control-Allow-Methods"] = "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
         else:
             response = await call_next(request)
         origin = request.headers.get("Origin", "")
         is_localhost = "localhost" in origin or "127.0.0.1" in origin
         if is_localhost:
-            response.headers["Access-Control-Allow-Origin"] = origin
-            response.headers["Access-Control-Allow-Methods"] = "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
             response.headers["Access-Control-Allow-Credentials"] = "true"
+            response.headers["Access-Control-Allow-Origin"] = origin
         return response
 
 
